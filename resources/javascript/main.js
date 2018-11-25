9e01d02be5b2f514d19b56db0cac5e9c44b7142c
M. Hogeveen <mhogeveen94@gmail.com>
2018-11-25 16:46:02 +0100

    Banner and slider complete

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $('.center').slick({
-  
+  $('.fotos').slick({
+    slidesToShow: 3,
+    swipe: true,
+    swipeToSlide: true,
+    //adaptiveWidth: true,
+    adaptiveHeight: true,
+    //centerMode: true,
+    //centerPadding: '20px',
+    //dots: true,
+    //arrows: true;
+    //fade: true,
   });
 });
